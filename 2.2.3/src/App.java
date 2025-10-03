d75b368ed7aa0dfc976e6df3ada7eb6f572540b0
Sento <Hieu.DM2416690@sis.hust.edu.vn>
2025-10-03 20:55:46 +0700

    Update App.java

--- 2.2.3/src/App.java
+++ 2.2.3/src/App.java
@@ -1,3 +1,10 @@
 import javax.swing.JOptionPane;
+
 public class HelloNameDialog {
-    public class }
+    public static void main(String[] args) {
+        String result;
+        result = JOptionPane.showInputDialog("Please enter your name:");
+        JOptionPane.showMessageDialog(null, "Hi " + result + "!");
+        System.exit(0);
+    }
+}
