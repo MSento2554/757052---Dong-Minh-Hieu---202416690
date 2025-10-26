4c7dbbbc9873e6babf03fb088914b8270f92bb06
MSento2554 <Hieu.DM2416690@sis.hust.edu.vn>
2025-10-26 15:57:40 +0700

    update DVD class

--- Aims project/Aims/src/DVD.java
+++ Aims project/Aims/src/DVD.java
@@ -1,63 +1,86 @@
 public class DVD {
-            private String title;
-            private String director;
-            private int length;
-            private float cost;
-            private String category;
-			public String getCategory() {
-				return category;
-			}
-			public void setCategory(String category) {
-				this.category = category;
-			}
-			public String getTitle() {
-				return title;
-			}
-			public void setTitle(String title) {
-				this.title = title;
-			}
-			public String getDirector() {
-				return director;
-			}
-			public void setDirector(String director) {
-				this.director = director;
-			}
-			public int getLength() {
-				return length;
-			}
-			public void setLength(int length) {
-				this.length = length;
-			}
-			public float getCost() {
-				return cost;
-			}
-			public void setCost(float cost) {
-				this.cost = cost;
-			}
-			public DVD(String title) {
-				super();
-				this.title = title;
-			}
-			public DVD(String title, float cost, String category) {
-				super();
-				this.title = title;
-				this.cost = cost;
-				this.category = category;
-			}
-			public DVD(String title, String director, float cost, String category) {
-				super();
-				this.title = title;
-				this.director = director;
-				this.cost = cost;
-				this.category = category;
-			}
-			public DVD(String title, String director, int length, float cost, String category) {
-				super();
-				this.title = title;
-				this.director = director;
-				this.length = length;
-				this.cost = cost;
-				this.category = category;
-			}
-            
-        }
+    private static int nbDigitalVideoDiscs = 0;
+    private int id;
+    private String title;
+    private String director;
+    private int length;
+    private float cost;
+    private String category;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public DVD(String title) {
+        this.title = title;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    public DVD(String title, float cost, String category) {
+        this(title);
+        this.cost = cost;
+        this.category = category;
+    }
+
+    public DVD(String title, String director, float cost, String category) {
+        this(title, cost, category);
+        this.director = director;
+    }
+
+    public DVD(String title, String director, int length, float cost, String category) {
+        this(title, director, cost, category);
+        this.length = length;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("DVD - [%s] - [%s] - [%s] - [%d]: %.2f $",
+                title, category, director, length, cost);
+    }
+
+    public boolean isMatch(String title) {
+        if (title == null || this.title == null) return false;
+        return this.title.equalsIgnoreCase(title.trim());
+    }
+}
